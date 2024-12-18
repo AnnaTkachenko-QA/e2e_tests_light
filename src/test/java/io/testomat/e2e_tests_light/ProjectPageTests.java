0a41d2fea3e61462b23059a3bc467139db97c684
AnnaTkachenko-QA <anna.tkachenko1603@gmail.com>
2024-12-18 21:02:36 +0200

    Lesson2 part1

--- src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
@@ -8,24 +8,47 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class ProjectPageTests {
 
+    String baseUrl = "https://app.testomat.io/";
+    String username = "anna.tkachenko1603@gmail.com";
+    String password = "Anna5624787249!";
+    String targetProjectName = "Manufacture light";
+
     @Test
     public void projectPageTest() {
-        open("https://app.testomat.io/");
+        open(baseUrl);
 
-        //login user
-        $("#content-desktop #user_email").setValue("anna.tkachenko1603@gmail.com");
-        $("#content-desktop #user_password").setValue("Anna5624787249!");
+        loginUser(username, password);
+
+        searchForProject(targetProjectName);
+
+        selectProject(targetProjectName);
+
+        waitForProjectPageIsLoaded(targetProjectName);
+    }
+
+    private void waitForProjectPageIsLoaded(String targetProjectName) {
+        $(".first h2").shouldHave(text(targetProjectName));
+        $(".first [href*='/readme']").shouldHave(text("Readme"));
+    }
+
+    private void selectProject(String targetProjectName) {
+        $(byText(targetProjectName)).click();
+    }
+
+    private void searchForProject(String targetProjectName) {
+        $("#content-desktop #search").setValue(targetProjectName);
+    }
+
+    @Test
+    public void newTest(){
+        open(baseUrl);
+    }
+
+    public void loginUser(String email, String password){
+        $("#content-desktop #user_email").setValue(email);
+        $("#content-desktop #user_password").setValue(password);
         $("#content-desktop #user_remember_me").click();
         $("#content-desktop [name=\"commit\"]").click();
         $(".common-flash-success").shouldBe(visible);
-
-        //search project
-        $("#content-desktop #search").setValue("Manufacture light");
-
-        //select project
-        $(byText("Manufacture light")).click();
-
-        //wait for project is loaded
-        $("h2").shouldHave(text("Manufacture light"));
     }
 }
