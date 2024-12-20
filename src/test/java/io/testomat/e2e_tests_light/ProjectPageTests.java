2e3ed3e43a7b068a544eb8f97c4713bba2728359
annatkachenko <anna.tkachenko1603@gmail.com>
2024-12-20 13:32:22 -0800

    Lesson 2

--- src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
@@ -1,29 +1,56 @@
 package io.testomat.e2e_tests_light;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static io.testomat.e2e_tests_light.utils.StringParsers.parseIntegerFromString;
 
-public class ProjectPageTests {
+public class ProjectPageTests extends BaseTest {
 
-    String baseUrl = "https://app.testomat.io/";
-    String username = "anna.tkachenko1603@gmail.com";
-    String password = "Anna5624787249!";
+    static String baseUrl = env.get("BASE_URL");
+    static String username = env.get("USERNAME");
+    static String password = env.get("PASSWORD");
     String targetProjectName = "Manufacture light";
 
-    @Test
-    public void projectPageTest() {
+    @BeforeAll
+    static void openTestomatAndLogin(){
         open(baseUrl);
 
         loginUser(username, password);
+    }
 
+    @BeforeEach
+    void openHomePage() {
+        open(baseUrl);
+    }
+
+    @Test
+    public void projectPageTest() {
         searchForProject(targetProjectName);
 
         selectProject(targetProjectName);
 
         waitForProjectPageIsLoaded(targetProjectName);
+    }
+
+    @Test
+    public void newTest() {
+        searchForProject(targetProjectName);
+
+        SelenideElement targetProject = countOfProjectsShouldBeEqualTo(1).first();
+
+        countOfTestCasesShouldBeEqualTo(targetProject, 0);
+
+        //totalCountOfTestCasesGraterThan(100);
     }
 
     private void waitForProjectPageIsLoaded(String targetProjectName) {
@@ -39,16 +66,29 @@ public class ProjectPageTests {
         $("#content-desktop #search").setValue(targetProjectName);
     }
 
-    @Test
-    public void newTest(){
-        open(baseUrl);
-    }
-
-    public void loginUser(String email, String password){
+    public static void loginUser(String email, String password) {
         $("#content-desktop #user_email").setValue(email);
         $("#content-desktop #user_password").setValue(password);
         $("#content-desktop #user_remember_me").click();
         $("#content-desktop [name=\"commit\"]").click();
         $(".common-flash-success").shouldBe(visible);
     }
+
+    private static void totalCountOfTestCasesGraterThan(int expectedTotalCount) {
+        String totalProjects = $("#container kbd").getText();
+        Integer actualCountOfTotalProjects = parseIntegerFromString(totalProjects);
+        Assertions.assertTrue(actualCountOfTotalProjects > expectedTotalCount);
+    }
+
+    private static void countOfTestCasesShouldBeEqualTo(SelenideElement targetProject, int expectedCount) {
+        String countOfTests = targetProject.$("p").getText();
+        Integer actualCountOfTests = parseIntegerFromString(countOfTests);
+        Assertions.assertEquals(expectedCount, actualCountOfTests);
+    }
+
+    @NotNull
+    private static ElementsCollection countOfProjectsShouldBeEqualTo(int expectedSize) {
+        return $$("#grid ul li").filter(visible).shouldHave(size(expectedSize));
+    }
+
 }
